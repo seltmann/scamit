e174895421a2a354c889c8e3338b3b0da70220f8
Enicospilus <Enicospilus@9464a796-6033-11de-bbaa-f1a44a2b9d47>
2009-06-25 13:53:49 +0000

    baseline

---
+++ trunk/_vti_cnf/fade.js
@@ -0,0 +1,8 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|Jay
+vti_modifiedby:SR|Jay
+vti_timecreated:TR|03 Jun 2001 17:25:24 -0000
+vti_timelastmodified:TR|03 Jun 2001 17:25:24 -0000
+vti_backlinkinfo:VX|contact.htm feedback.htm newsletter.htm about_scamit.htm edition_four_information.htm index.htm map.htm new_related_links.htm pub.htm latest_news.htm policy.htm taxonomic_tools.htm
+vti_extenderversion:SR|4.0.2.2717
+vti_syncwith_www.scamit.org\:80:TR|03 Jun 2001 17:25:24 -0000
